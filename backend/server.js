1102de78740be42631cb1dcf08350f383035bca3
Anurag Wadhwa <anuragwadhwa@Anurags-MacBook-Air.local>
2025-03-28 22:56:47 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -9,36 +9,15 @@ const userRoutes = require("./routes/UserRoutes");
 
 const { app, server } = require("./socket/Socket");
 
-const allowCors = (req, res, next) => {
 
-    res.setHeader("Access-Control-Allow-Origin", "https://chatify-talks.vercel.app"); // Default
-
-
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
-    );
-
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200); // ✅ Handles preflight requests
-    }
-
-    next(); // ✅ Continue to the next middleware
-};
-
-// ✅ Apply CORS Middleware
-app.use(allowCors);
-
-// app.use(cors({ origin: ['https://chatify-talks.vercel.app','http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: "https://chatify-talks.vercel.app", credentials: true }));
 app.use(express.json());
 
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
     console.log(req.headers)
-    res.status(200).json({ message: "Server is up and running!" + `${req.headers.host}` });
+    res.status(200).json({ message: "Server is up and running!" });
 });
 
 
